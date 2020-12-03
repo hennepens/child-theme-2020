3a5e46ddb5a52c3e43cef1b77c2f2bd3f1581c09
Lab Society <eric@labsociety.com>
2020-12-03 09:53:08 -0500

    fixing misc. styles

--- functions.php
+++ functions.php
@@ -612,23 +612,4 @@ echo'</span>';
 endif; 
 } 
 
-// Remove "Select options" button from (variable) products on the main WooCommerce shop page.
-add_filter( 'woocommerce_loop_add_to_cart_link', function( $product ) {
-
-  global $product;
-
-  if ( is_shop() && 'variable' === $product->product_type ) {
-    return '';
-  } else {
-    sprintf( '<a href="%s" data-quantity="%s" class="%s" %s>%s</a>',
-      esc_url( $product->add_to_cart_url() ),
-      esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
-      esc_attr( isset( $args['class'] ) ? $args['class'] : 'button' ),
-      isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
-      esc_html( $product->add_to_cart_text() )
-    );
-  }
-
-} );
-
 ?>
