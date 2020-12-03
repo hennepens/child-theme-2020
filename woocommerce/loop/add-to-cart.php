e3b1009d89a69df00ae30d9dd6a9fa049804ac5b
Lab Society <eric@labsociety.com>
2020-12-03 18:46:30 -0500

    fixing misc. styles

--- woocommerce/loop/add-to-cart.php
+++ woocommerce/loop/add-to-cart.php
@@ -20,11 +20,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 global $product;
-$productUrl = get_page_by_title( 'Product Title', OBJECT, 'product' );
+
 echo apply_filters(
 	'woocommerce_loop_add_to_cart_link', // WPCS: XSS ok.
 	sprintf(
-		'<a href="%s" data-quantity="%s" class="%s" %s>%s</a> <a class="subscription-message" href="'. get_permalink( $productUrl->ID ) .'">Subscribe &amp; Save 40&percnt;</a>',
+		'<a href="%s" data-quantity="%s" class="%s" %s>%s</a>',
 		esc_url( $product->add_to_cart_url() ),
 		esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
 		esc_attr( isset( $args['class'] ) ? $args['class'] : 'button' ),
