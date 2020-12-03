f213ee94969d0caedf9eb3e608567d4e194e241e
Lab Society <eric@labsociety.com>
2020-12-03 01:48:05 -0500

    fixing misc. styles

--- js/child-theme.js
+++ js/child-theme.js
@@ -7055,4 +7055,12 @@
 	  $(this).find('span').text($(this).find('span').text() == 'Sign Up Now' ? 'Login' : 'Sign Up Now');
 	  $(this).siblings('.lines').find('span').text($(this).siblings('.lines').find('span').text() == 'New to Hennepen\'s?' ? 'Already a Member?' : 'New to Hennepen\'s?');
 	});
+	
+	$( document ).ready(function() {
+		var cbdReplace = $('.products li.product h2').html();
+		cbdReplace = cbdReplace.replace('CBD', 'CBD<br/>');
+		cbdReplace2 = cbdReplace.replace('Hemp', 'Hemp<br/>');
+		$('.products li.product h2').html(cbdReplace);
+		$('.products li.product h2').html(cbdReplace2);
+	});
 })( jQuery );
