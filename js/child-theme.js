a6d90981edbd85a651ed8907e25e40e94b08e1ae
Lab Society <eric@labsociety.com>
2020-12-07 10:15:05 -0500

    fixing misc. styles

--- js/child-theme.js
+++ js/child-theme.js
@@ -7056,8 +7056,10 @@
 	  $(this).siblings('.lines').find('span').text($(this).siblings('.lines').find('span').text() == 'New to Hennepen\'s?' ? 'Already a Member?' : 'New to Hennepen\'s?');
 	});
 
-	$('a.video-banner').click(function(e) {
-    	e.preventDefault();
+	$('.video-banner a').click(function(e) {
+		e.preventDefault();
+		console.log('Clicked the video banner!');
+    	
   	$('#video_container').html('<iframe src="http://player.vimeo.com/video/485224796?title=1&amp;byline=1&amp;portrait=1&amp;autoplay=true" width="100%" height="720" frameborder="0"></iframe>');
   
 });
