1f40d751c1ee0a9ac9b6581f7e0e432e95fe88f0
Lab Society <eric@labsociety.com>
2020-12-06 23:48:26 -0500

    fixing misc. styles

--- footer.php
+++ footer.php
@@ -19,10 +19,10 @@ $container = get_theme_mod( 'understrap_container_type' );
 		<div class="row">
 			<div class="col-md-9">
 				<div class="row">
-					<div class="d-flex col-2 justify-content-center align-items-md-start">
+					<div class="d-flex col-md-2 justify-content-center align-items-md-start">
 						<img id="logo-svg" class="mb-3" src="<?php echo get_stylesheet_directory_uri(); ?>/images/hennepens-logomark.svg" alt="Hennepen's" width="80">
 					</div>
-					<div class="col-10">
+					<div class="col-md-10">
 						<div class="row">
 							<div class="col-md-8">
 								<?php wp_nav_menu( array('menu' => 'Main Footer Menu') ); ?>
