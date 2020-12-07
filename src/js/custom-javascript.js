264223ea0b643a96ca9bc8a2d0a2ae8b537a5c50
Lab Society <eric@labsociety.com>
2020-12-07 10:15:45 -0500

    fixing misc. styles

--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -12,7 +12,7 @@
 		e.preventDefault();
 		console.log('Clicked the video banner!');
     	
-  	$('#video_container').html('<iframe src="http://player.vimeo.com/video/485224796?title=1&amp;byline=1&amp;portrait=1&amp;autoplay=true" width="100%" height="720" frameborder="0"></iframe>');
+  	$('#video_container').html('<iframe src="https://player.vimeo.com/video/485224796?title=1&amp;byline=1&amp;portrait=1&amp;autoplay=true" width="100%" height="720" frameborder="0"></iframe>');
   
 });
 
