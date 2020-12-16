4d800d06c8337efebcfef9cae4f7d5eeb934dbfb
Lab Society <eric@labsociety.com>
2020-12-16 07:52:44 -0500

    optimizing speed

--- functions.php
+++ functions.php
@@ -8,11 +8,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 function add_slug_body_class( $classes ) {
 global $post;
 if ( isset( $post ) ) {
-$classes[] = $post->post_type . '-' . $post->post_name . " fade-out";
+$classes[] = $post->post_type . '-' . $post->post_name;
 }
 return $classes;
 }
-//add_filter( 'body_class', 'add_slug_body_class' );
+add_filter( 'body_class', 'add_slug_body_class' );
 
 
 function page_load_script(){
