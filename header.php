2093ebe8db733091aeb149629ff04168a6121c7b
Lab Society <eric@labsociety.com>
2020-12-18 11:21:00 -0500

    Optimizing fonts

--- header.php
+++ header.php
@@ -19,9 +19,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 		<!-- Main font -->
-	<link rel="preload" as="font" href="<?php echo get_stylesheet_directory_uri();?>/fonts/librebaskerville-regular-webfont.woff2" type="font/woff2" crossorigin="anonymous">
 	<link rel="preload" as="font" href="<?php echo get_stylesheet_directory_uri();?>/fonts/librebaskerville-bold-webfont.woff2" type="font/woff2" crossorigin="anonymous">
-	<link rel="preload" as="font" href="<?php echo get_stylesheet_directory_uri();?>/fonts/librebaskerville-italic-webfont.woff2" type="font/woff2" crossorigin="anonymous">
 	<!-- Icon font -->
 	<link rel="preload" as="font" href="<?php echo get_stylesheet_directory_uri();?>/fonts/linearicons.woff" type="font/woff" crossorigin="anonymous">
 
