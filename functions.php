c77a1503ea5d6a7793660c9a741936e8021d2e6e
labsociety <eric@labsociety.com>
2021-05-30 07:37:04 -0400

    fixing catalog title id issue

--- functions.php
+++ functions.php
@@ -183,7 +183,8 @@ function custom_single_product_title_inject(){
 }
 
 function custom_loop_product_title_inject(){
-  $product_main_title = get_post_meta(get_the_id(), '_bhww_main_title_wysiwyg', true );
+  global $product;
+  $product_main_title = get_post_meta( $product->get_id(), '_bhww_main_title_wysiwyg', true );
 
     if ( !empty( $product_main_title ) ) {
         echo '<h2 class="woocommerce-loop-product__title">' . $product_main_title . '</h2>';
