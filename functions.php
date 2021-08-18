422942d2b6097cb1a86d86b1542eb3d4e41acf31
Eric Gang <ericgang@Erics-MacBook-Pro.local>
2021-08-18 11:14:34 -0400

    added coupon code functionality to url

--- functions.php
+++ functions.php
@@ -1105,4 +1105,43 @@ return $items;
    
 add_filter( 'woocommerce_account_menu_items', 'bbloomer_add_premium_support_link_my_account' );
 
+function webroom_woocommerce_coupon_links(){
+
+  // Bail if WooCommerce or sessions aren't available.
+
+  if (!function_exists('WC') || !WC()->session) {
+    return;
+  }
+
+  /**
+   * Filter the coupon code query variable name.
+   *
+   * @since 1.0.0
+   *
+   * @param string $query_var Query variable name.
+   */
+  $query_var = apply_filters('woocommerce_coupon_links_query_var', 'coupon_code');
+
+  // Bail if a coupon code isn't in the query string.
+
+  if (empty($_GET[$query_var])) {
+    return;
+  }
+
+  // Set a session cookie to persist the coupon in case the cart is empty.
+
+  WC()->session->set_customer_session_cookie(true);
+
+  // Apply the coupon to the cart if necessary.
+
+  if (!WC()->cart->has_discount($_GET[$query_var])) {
+
+    // WC_Cart::add_discount() sanitizes the coupon code.
+
+    WC()->cart->add_discount($_GET[$query_var]);
+  }
+}
+add_action('wp_loaded', 'webroom_woocommerce_coupon_links', 30);
+add_action('woocommerce_add_to_cart', 'webroom_woocommerce_coupon_links');
+
 ?>
