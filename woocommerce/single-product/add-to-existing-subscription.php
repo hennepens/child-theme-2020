ae1ae661883684ba96ceaf747c9e4e2a6954fbc6
Eric Gang <ericgang@Erics-MacBook-Pro.local>
2021-08-28 19:22:20 -0400

    fixing add existing product ot subscription

--- woocommerce/single-product/add-to-existing-subscription.php
+++ woocommerce/single-product/add-to-existing-subscription.php
@@ -44,6 +44,6 @@ if ( ! $product->is_purchasable() ) {
 		<?php echo esc_html_x( 'Add to existing subscription', 'Text on button for add to existing subscription functionality', 'jg-toolbox' ); ?>
 	</button>
 	<script>
-		document.getElementById("atsval").value = "1";
+		document.getElementById("atsval").value = 1;
 	</script>
 </form>
