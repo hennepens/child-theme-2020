7c8d8f297f951457826e77cde1a34dc82cb9269f
Eric Gang <ericgang@Erics-MacBook-Pro.local>
2021-08-30 08:21:16 -0400

    cleaning up subscription quickview and single product page styles

--- functions.php
+++ functions.php
@@ -320,10 +320,7 @@ function get_help_icon($content, $type = 'text', $echo = false){
       }
 
     });
-      var savetext = jQuery(".subscription-price");
-      jQuery(":contains(\'— save\')").each(function(){
-        jQuery(savetext).html(jQuery(savetext).html().split("— save").join(""));
-      });
+
       
       
         // purchase options
