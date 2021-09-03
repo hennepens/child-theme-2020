9ede8d02e3a81ea0190681f517364f5f15fd9d58
Eric Gang <ericgang@Erics-MacBook-Pro.local>
2021-09-03 13:14:05 -0400

    adding google pay

--- footer.php
+++ footer.php
@@ -80,7 +80,9 @@ $container = get_theme_mod( 'understrap_container_type' );
 									&copy; <?php echo date_i18n(_x( 'Y', 'copyright date format', 'understrap' )); ?> Hennepen&apos;s&reg;. All Rights Reserved.
 								</li>
 							</ul>
-
+							 <!-- (c) 2005, 2021. Authorize.Net is a registered trademark of CyberSource Corporation -->
+							 <div class="AuthorizeNetSeal m-auto pb-4"> <script type="text/javascript" language="javascript">var ANS_customer_id="73988ae6-bab4-4a12-b67d-f3c5edff17a3";</script> <script type="text/javascript" language="javascript" src="//verify.authorize.net:443/anetseal/seal.js" ></script>
+							 </div>
 							<ul class="justify-content-center">
 								<li>
 									<a href="/terms-of-service">Terms of Service</a>
