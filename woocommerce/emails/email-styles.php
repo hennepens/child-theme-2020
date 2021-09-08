20d254c1f5686549b4c87269147b278284c2683f
Eric Gang <ericgang@Erics-MacBook-Pro.local>
2021-09-08 11:15:05 -0400

    adding dark mode support to email template

--- woocommerce/emails/email-styles.php
+++ woocommerce/emails/email-styles.php
@@ -270,19 +270,19 @@ img {
 
 @media (prefers-color-scheme: dark ) {
   /* Shows Dark Mode-Only Content, Like Images */
-  #dark-img { display:block !important; width: auto !important; overflow: visible !important; float: none !important; max-height:inherit !important; max-width:inherit !important; line-height: auto !important; margin-top:0px !important; visibility:inherit !important; margin-top: 40px; text-align: center;}
+  .dark-img { display:block !important; width: auto !important; overflow: visible !important; float: none !important; max-height:inherit !important; max-width:inherit !important; line-height: auto !important; margin-top:0px !important; visibility:inherit !important; margin-top: 40px; text-align: center;}
    
   /* Hides Light Mode-Only Content, Like Images */
-  #light-img { display:none; display:none !important; }
+  .light-img { display:none; display:none !important; }
    
 }
 
 @media (prefers-color-scheme: light ) {
   /* Shows light Mode-Only Content, Like Images */
-  #light-img { display:block !important; width: auto !important; overflow: visible !important; float: none !important; max-height:inherit !important; max-width:inherit !important; line-height: auto !important; margin-top:0px !important; visibility:inherit !important; margin-top: 40px; text-align: center; }
+  .light-img { display:block !important; width: auto !important; overflow: visible !important; float: none !important; max-height:inherit !important; max-width:inherit !important; line-height: auto !important; margin-top:0px !important; visibility:inherit !important; margin-top: 40px; text-align: center; }
    
   /* Hides Light Mode-Only Content, Like Images */
-  #dark-img { display:none; display:none !important; }
+  .dark-img { display:none; display:none !important; }
 }
 
 <?php
