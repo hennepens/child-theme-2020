682c0a2bf9b00f211866aa7ff978461b8c1913b0
Eric Gang <ericgang@Erics-MBP.home>
2021-09-18 07:06:30 -0400

    remove -save from savings text

--- functions.php
+++ functions.php
@@ -388,8 +388,8 @@ function get_help_icon($content, $type = 'text', $echo = false){
 
         jQuery("body").removeClass("clicked-subscription");
       }
-      jQuery(".subscription-price").find(\'&mdash; save\').each(function(){
-        jQuery(this).text(jQuery(this).text().replace(\'&mdash; save\',\' \'));
+      jQuery(".subscription-price").find(\'&#8212; save\').each(function(){
+        jQuery(this).text(jQuery(this).html().replace(\'&#8212; save\',\' \'));
       });
 
   };
