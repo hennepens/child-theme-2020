c79bbc0223330f4109b59dc3c4a5e48a1446490b
Eric Gang <ericgang@Erics-MacBook-Pro.local>
2021-12-20 08:58:26 -0500

    updated selection js on single product page

--- woocommerce/single-product/product-subscription-options.php
+++ woocommerce/single-product/product-subscription-options.php
@@ -16,7 +16,96 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 ?>
+<script>
+(function($){
+    $(document).ready(function(){
+		 var selectedVariant = $(".generatedRadios.selected label input").val();
+		 selectedVariant = selectedVariant.split(" ")[0];
+		 $(".current_selected_variant").text(selectedVariant);
+    	$(".input-text.qty").change(function(){
+    		var selectedVariant = $(".generatedRadios.selected label input").val();
+    		var selectedQty = $(this).children("option:selected").val();
+		  $(".current_selected_variant").text(selectedVariant);
+		  $(".quantity #realQty").val(selectedQty);
+		  $(".delivery-every .current_selected_qty").text(selectedQty);
+		  	if(selectedQty <= 1){
+		  		selectedVariant = selectedVariant.split(" ")[0];
+		  		$(".current_selected_variant").text(selectedVariant);
+			}else if(selectedQty>=2){
+				if(selectedVariant == "Box"){
+					$(".current_selected_variant").text(selectedVariant + "es");
+				}
+				else{
+					selectedVariant = selectedVariant.split(" ")[0];
+					$(".current_selected_variant").text(selectedVariant + "s");
+				}
+			}
+		});
+    	var $savetext = $(".subscription-price");
+    	
+    	$(':contains("— save")').each(function(){
+		    $($savetext).html($($savetext).html().split("— save").join(""));
+		});
+    	
+        // purchase options
+		$('.purchase-options input').change(function(){
 
+			if ($(this).val() == 'one-time') updatePurchaseOptions(0);
+			else updatePurchaseOptions($('.purchase-options select').val());
+
+			$(this).closest('li').addClass('selected')
+				.siblings().removeClass('selected');
+
+		});
+
+		$('.wcsatt-options-wrapper .wcsatt-options-product input').change(function(){
+
+			if ($(this).val() == 'one-time') updatePurchaseOptions(0);
+			else updatePurchaseOptions($('.wcsatt-options-wrapper .wcsatt-options-product select').val());
+
+			$(this).siblings('.purchase-options .selected .subscriptions-list .subscription-price').children('li').addClass('selected')
+				.siblings().removeClass('selected');
+
+		});
+
+
+
+		$('.purchase-options select').change(function(){
+			$('.purchase-options input[value="subscription"]').prop('checked', true).change();
+		});
+
+		function updatePurchaseOptions(v){
+			$('.wcsatt-options-product input[value="'+ v +'"]').prop('checked', true).change();
+		}
+		if( jQuery( ".variations_form select" ).length  ){
+
+			// get json value from woocomerce from
+		    
+			var product_attr    =   jQuery.parseJSON( $(".variations_form").attr("data-product_variations") ),
+		    	obj_attr		= "";
+
+			
+		    jQuery( ".variations_form select" ).on( "change", function () {		        
+		       // Create New Array by selecting variations
+		        jQuery( ".variations_form select" ).each(function( index ) {
+		            
+		             obj_attr[ $(this).attr("name") ] = $(this).val();
+		            
+		        });
+		        
+		        // Get Variations
+		        jQuery.each( product_attr, function( index, loop_value ) {
+		        
+		            if( JSON.stringify( obj_attr ) === JSON.stringify( loop_value.attributes )  ){
+		                $('.one-time-price').html( loop_value.price_html );
+		                
+		            }
+		        }); 
+		    });
+		}
+    });
+})(jQuery);
+</script>
 <?php
 
 // exit if accessed directly
@@ -58,23 +147,12 @@ foreach ($options as $option) {
 }
 //$amount_saved = $product->get_sale_price();
 echo
-"
-<script>
-	jQuery(function($) {
-        if(jQuery('body').hasClass('woofunnels-pop')) {
-            console.log('got here!');
-            jQuery('.choose-label .subscribe-prompt').text(jQuery('.choose-label .subscribe-prompt').text().replace('Subscribe & Save 15%', 'Subscribe & Save 20%'));
-           	jQuery('body').removeClass('woofunnels-pop');
-        }
-        });
-        </script>
-
-<div class='qty-container'><label>Select a Quantity</label><select id='selectQty' class='input-text qty'><option value='1'>1</option><option value='2'>2</option><option value='3'>3</option><option value='4'>4</option><option value='5'>5</option><option value='6'>6</option><option value='7'>7</option><option value='8'>8</option><option value='9'>9</option></select></div><label>I would like to</label><ul class='purchase-options'>\n".
+"<div class='qty-container'><label>Select a Quantity</label><select id='selectQty' class='input-text qty'><option value='1'>1</option><option value='2'>2</option><option value='3'>3</option><option value='4'>4</option><option value='5'>5</option><option value='6'>6</option><option value='7'>7</option><option value='8'>8</option><option value='9'>9</option></select></div><label>I would like to</label><ul class='purchase-options'>\n".
 	(isset($one_time_option) ? $one_time_option : "").
 
 	($subscription_options ? "<li". (isset($selected_subscription_option) ? " class='selected'" : "") .">".
 		"<input type='radio' id='subscriptions-list' name='purchase-options' value='subscription'". (isset($selected_subscription_option) ? " checked" : "") ." />\n".
-		"<label for='subscriptions-list' class='subscription-container'><span class='choose-label'><span class='subscribe-prompt'>". ($prompt ? strip_tags($prompt) : __('Subscribe & Save 15%')) . "</span><ul class='subscription-price'>". implode('', $hidden_options) ."</ul></label>\n".
+		"<label for='subscriptions-list' class='subscription-container'><span class='choose-label'>". ($prompt ? strip_tags($prompt) : __('Subscribe & Save 15%')) . "<ul class='subscription-price'>". implode('', $hidden_options) ."</ul></label>\n".
 		"<div class='delivery-container'><h6 class='prompt-heading'>Choose a Subscription Cycle</h6><div class='subscription-delivery'><label class='delivery-every'>Deliver <span class='current_selected_qty'>1</span> <span class='current_selected_variant'></span> every</label><select name='subscription-options'>". implode('', $subscription_options) ."</select>\n" : "").
 		get_help_icon($tip_text) ."</div></div></li>\n".
 "</li>".
